b57bfcfc3ce6f754bcbf3e266516dc54b01c4aac
Dapeng Wei <tiwheeharniver@outlook.com>
2026-01-03 02:09:15 +0530

    update

---
+++ hdwallet/eccs/kholaw/ed25519/point.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Copyright © 2020-2025, Meheret Tesfaye Batu <meherett.batu@gmail.com>
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit
+
+from ...slip10.ed25519 import SLIP10Ed25519Point
+
+
+class KholawEd25519Point(SLIP10Ed25519Point):
+
+    @staticmethod
+    def name() -> str:
+        """
+        Get the name of the ecc class.
+
+        :return: The name of the ecc class.
+        :rtype: str
+        """
+
+        return "Kholaw-Ed25519"
