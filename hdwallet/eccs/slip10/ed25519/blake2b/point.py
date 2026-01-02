631d4fdaf9e79dc057007e840395dfe652e9efc2
Dapeng Wei <tiwheeharniver@outlook.com>
2026-01-03 02:09:34 +0530

    update

---
+++ hdwallet/eccs/slip10/ed25519/blake2b/point.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Copyright © 2020-2025, Meheret Tesfaye Batu <meherett.batu@gmail.com>
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit
+
+from ..point import SLIP10Ed25519Point
+
+
+class SLIP10Ed25519Blake2bPoint(SLIP10Ed25519Point):
+
+    @staticmethod
+    def name() -> str:
+        """
+        Get the name of the ecc class.
+
+        :return: The name of the ecc class.
+        :rtype: str
+        """
+
+        return "SLIP10-Ed25519-Blake2b"
