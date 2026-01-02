2aea2ebd1d7f2b3f8abb9d271c8f1d3bc2432997
Dapeng Wei <tiwheeharniver@outlook.com>
2026-01-03 02:10:20 +0530

    update

---
+++ hdwallet/entropies/electrum/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Copyright © 2020-2024, Meheret Tesfaye Batu <meherett.batu@gmail.com>
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit
+
+from typing import List
+
+from .v1 import (
+    ElectrumV1Entropy, ELECTRUM_V1_ENTROPY_STRENGTHS
+)
+from .v2 import (
+    ElectrumV2Entropy, ELECTRUM_V2_ENTROPY_STRENGTHS
+)
+
+
+__all__: List[str] = [
+    "ElectrumV1Entropy",
+    "ELECTRUM_V1_ENTROPY_STRENGTHS",
+    "ElectrumV2Entropy",
+    "ELECTRUM_V2_ENTROPY_STRENGTHS"
+]
