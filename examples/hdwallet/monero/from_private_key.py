8a4b72a3ba225d8cbda20805f5ca78af35b02cac
Dapeng Wei <tiwheeharniver@outlook.com>
2026-01-03 02:06:09 +0530

    update

---
+++ examples/hdwallet/monero/from_private_key.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from hdwallet import HDWallet
+from hdwallet.cryptocurrencies import Monero as Cryptocurrency
+from hdwallet.derivations import MoneroDerivation
+from hdwallet.hds import MoneroHD
+
+import json
+
+
+hdwallet: HDWallet = HDWallet(
+    cryptocurrency=Cryptocurrency,
+    hd=MoneroHD,
+    network=Cryptocurrency.NETWORKS.MAINNET,
+    payment_id="ad17dc6e6793d178"
+).from_private_key(
+    private_key="0fea3ff3b19b033672e8ac8a3b26fed252daf30762c8294e9dd62dc417d2108e"
+).from_derivation(
+    derivation=MoneroDerivation(
+        minor=0, major=5
+    )
+)
+
+print(json.dumps(hdwallet.dump(exclude={"indexes"}), indent=4, ensure_ascii=False))
+# print(json.dumps(hdwallet.dumps(exclude={"indexes"}), indent=4, ensure_ascii=False))
+
+# print("Cryptocurrency:", hdwallet.cryptocurrency())
+# print("Symbol:", hdwallet.symbol())
+# print("Network:", hdwallet.network())
+# print("Coin Type:", hdwallet.coin_type())
+# print("ECC:", hdwallet.ecc())
+# print("HD:", hdwallet.hd())
+# print("Private Key:", hdwallet.private_key())
+# print("Spend Private Key:", hdwallet.spend_private_key())
+# print("View Private Key:", hdwallet.view_private_key())
+# print("Spend Public Key:", hdwallet.spend_public_key())
+# print("View Public Key:", hdwallet.view_public_key())
+# print("Primary Address:", hdwallet.primary_address())
+# print("Integrated Address:", hdwallet.integrated_address())
+# print("Sub Address:", hdwallet.sub_address())
